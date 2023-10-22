13ebbfcd0351ed6486955b8caafee31b7b3214c2
Muhammad-Shahid-Jamal <shahid.jamal0343@gmail.com>
2023-10-22 21:12:39 +0500

    gitignore changes reverted and replace simple div in theme section

--- src/sections/ThemesSection/ThemesSection.jsx
+++ src/sections/ThemesSection/ThemesSection.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import './ThemesSection.css';
-import { motion } from 'framer-motion';
 
 const ThemesSection = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1, transition: { duration: 1 } }}
-      viewport={{ once: false }}
-      className="hack4bengal__section5"
-    >
-      <div className="h4b_about_main">
-        <div id="themes">
-          <div>
-            <h1>Themes</h1>
-            <hr />
-            <h3 style={{ textAlign: 'center' }}>To be decided soon...</h3>
-          </div>
+    <div className="h4b_about_main">
+      <div id="themes">
+        <div>
+          <h1>Themes</h1>
+          <hr />
+          <h3 style={{ textAlign: 'center' }}>To be decided soon...</h3>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
