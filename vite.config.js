23d4fd5aee5340fe41df5436f07be374cd5a7076
Tamal Das <tamalcodes@gmail.com>
2023-10-27 00:19:23 +0530

    fix: changes to config plugins

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the output fir from dist to build
-  // comment this out if that isn't relevent for your project
-  build: {
-    outDir: "build",
+  plugins: [svgr(), react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
   },
-  plugins: [
-    reactRefresh(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
-      },
-    }),
-  ],
+  watch: {
+    usePolling: true,
+  },
 });
