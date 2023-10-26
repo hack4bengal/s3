2a2ac135d5450030b41726298f7cec3366b56cdf
rohits-web03 <rohit.dey302003@gmail.com>
2023-10-27 05:14:28 +0530

    Created PastSponsors.js to store the data of the images in a single file

---
+++ src/assets/data/PastSponsors.js
@@ -0,0 +1,314 @@
+import cscoe from "../images/sponsers/cscoe3.png";
+import wb from "../images/sponsers/wb3.png";
+import nasscom from "../images/sponsers/nasscom.png";
+import tte from "../images/sponsers/tte4.png";
+import xyz from "../images/sponsers/xyz-logo-white.png";
+import taskade from "../images/sponsers/taskade.png";
+import echo3D from "../images/sponsers/echo3D.png";
+import axure from "../images/sponsers/axure.png";
+import postman from "../images/sponsers/Postman.png";
+import at from "../images/sponsers/at.png";
+import foss from "../images/sponsers/foss.png";
+import egk from "../images/sponsers/egk.png";
+import balsamiq from "../images/sponsers/balsamiq.png";
+import ccd from "../images/sponsers/CCD.png";
+import polygon from "../images/sponsers/polygon.png";
+import replit from "../images/sponsers/replit.png";
+import solana from "../images/sponsers/solana.png";
+import shardeum from "../images/sponsers/shardeum.png";
+import fcf from "../images/sponsers/fcf.png";
+import fvm from "../images/sponsers/fvm.png";
+import hellenergy from "../images/sponsers/hell-energy.png";
+import gdgKolkata from "../images/sponsers/gdgcloudkolkata.png";
+import intel from "../images/sponsers/intel.png";
+import celo from "../images/sponsers/celo.jpg";
+import tezos from "../images/sponsers/tezos.png";
+import github from "../images/sponsers/github-text-white.png";
+import gmc from "../images/sponsers/gmc.png";
+import digitalocean from "../images/sponsers/digitalocean.jpg"
+import hyperlane from "../images/sponsers/hyperlane.png";
+import flow from "../images/sponsers/flow.png";
+import devfolio from "../images/sponsers/Devfolio.png";
+import logitech from "../images/sponsers/logitech.png";
+import quine from "../images/sponsers/quine.svg";
+import snu from "../images/sponsers/snu.png";
+import zeeve from "../images/sponsers/zeeve.png";
+import graph from "../images/sponsers/Graphlogo1.png";
+import Resourcio from "../images/sponsers/Resourcio_LOGO.jpg";
+import Devorld from "../images/sponsers/Devorld_LOGO.png";
+import IEIAOT from "../images/sponsers/IEIAOT_LOGO.png";
+import CodeChamp from "../images/sponsers/CodeChamp_LOGO.jpg";
+import password from "../images/sponsers/1p.jpg";
+import interviewcake from "../images/sponsers/interview-cake.png";
+import LL from "../images/sponsers/leading-learners.png";
+import auth from "../images/sponsers/AuthO.png";
+import capitalone from "../images/sponsers/CapitalOne.png";
+import Deso from "../images/sponsers/Deso.png";
+import GoDaddy from "../images/sponsers/GoDaddy.png";
+import hedera from "../images/sponsers/Hedera.png";
+import linode from "../images/sponsers/Linode.png";
+import twilio from "../images/sponsers/Twilio.png";
+import stickermule from "../images/sponsers/stickermule.png";
+
+const gov=[
+    {
+        img: cscoe,
+        link: "https://cscoe.itewb.gov.in/",
+        alt: "Cybersecurity Centre of execellence",
+    },
+    {
+        img: wb,
+        link: "https://wb.gov.in/",
+        alt: "Government of West Bengal",
+    },
+]
+
+const sponsors=[
+    {
+        img: nasscom,
+        link: "https://nasscom.in/",
+        alt: "nasscom",
+    },
+    {
+        img: tte,
+        link: "https://www.telegraphindia.com/edugraph",
+        alt: "Telegraph India",
+    },
+    {
+        img: xyz,
+        link: "https://gen.xyz/",
+        alt: ".XYZ",
+    },
+    {
+        img: taskade,
+        link: "https://www.taskade.com/",
+        alt: "Taskade",
+    },
+    {
+        img: echo3D,
+        link: "https://www.echo3d.com/",
+        alt: "Echo3D",
+    },
+    {
+        img: axure,
+        link: "https://www.axure.com/",
+        alt: "Axure",
+    },
+    {
+        img: postman,
+        link: "https://www.postman.com/",
+        alt: "Postman",
+    },
+    {
+        img: at,
+        link: "https://atsign.com/",
+        alt: "The @ Company",
+    },
+    {
+        img: stickermule,
+        link: "https://www.stickermule.com/unlock?ref_id=0539570701&utm_source=sponsorship&utm_campaign=mlh-sponsorship-2019&utm_medium=referral",
+        alt: "Sticker Mule",
+    },
+    {
+        img: foss,
+        link: "https://fossunited.org/",
+        alt: "Foss United",
+    },
+    {
+        img: egk,
+        link: "https://m.esogolpokori.co.in/",
+        alt: "Eso Golpo Kori",
+    },
+    {
+        img: balsamiq,
+        link: "https://balsamiq.com/",
+        alt: "Balsamiq",
+    },
+    {
+        img: ccd,
+        link: "https://www.cafecoffeeday.com/",
+        alt: "Cafe Coffee Day",
+    },
+    {
+        img: polygon,
+        link: "https://polygon.technology/",
+        alt: "Polygon",
+    },
+    {
+        img: replit,
+        link: "https://replit.com/",
+        alt: "Replit",
+    },
+    {
+        img: solana,
+        link: "https://solana.com/",
+        alt: "Solana",
+    },
+    {
+        img: shardeum,
+        link: "https://shardeum.org/",
+        alt: "Shardeum",
+    },
+    {
+        img: fcf,
+        link: "https://filecoin.io/",
+        alt: "FILECOIN LOGO",
+    },
+    {
+        img: fvm,
+        link: "https://fvm.filecoin.io/",
+        alt: "Filecoin Virtual Machine",
+    },
+    {
+        img: hellenergy,
+        link: "https://www.hellenergy.com/",
+        alt: "Hell Energy",
+    },
+    {
+        img: gdgKolkata,
+        link: "https://gdg.community.dev/gdg-kolkata/",
+        alt: "GDG Kolkata",
+    },
+    {
+        img: intel,
+        link: "https://www.intel.com/content/www/us/en/homepage.html",
+        alt: "Intel",
+    },
+    {
+        img: celo,
+        link: "https://celo.org/",
+        alt: "Celo",
+    },
+    {
+        img: tezos,
+        link: "https://tezos.com/",
+        alt: "Tezos",
+    },
+    {
+        img: github,
+        link: "https://github.com/",
+        alt: "Github",
+    },
+    {
+        img: gmc,
+        link: "https://givemycertificate.com/",
+        alt: "Give My Certificate",
+    },
+    {
+        img: digitalocean,
+        link: "https://www.digitalocean.com/",
+        alt: "Digital Ocean",
+    },
+    {
+        img: hyperlane,
+        link: "https://www.hyperlane.xyz/",
+        alt: "Hyperlane",
+    },
+    {
+        img: flow,
+        link: "https://flow.com/",
+        alt: "Flow",
+    },
+    {
+        img: devfolio,
+        link: "https://devfolio.co/discover",
+        alt: "Devfolio",
+    },
+    {
+        img: logitech,
+        link: "https://www.logitech.com/en-in",
+        alt: "Logitech",
+    },
+    {
+        img: quine,
+        link: "https://quine.sh/",
+        alt: "Quine",
+    },
+    {
+        img: snu,
+        link: "https://snuniv.ac.in/",
+        alt: "Sister Nivedita University",
+    },
+    {
+        img: zeeve,
+        link: "https://www.zeeve.io/",
+        alt: "Zeeve",
+    },
+    {
+        img: graph,
+        link: "https://thegraph.com/",
+        alt: "The graph",
+    },
+    {
+        img: Resourcio,
+        link: "https://www.resourcio.tech/",
+        alt: "Resourcio",
+    },
+    {
+        img: Devorld,
+        link: "https://in.linkedin.com/company/devorld",
+        alt: "Devorld",
+    },
+    {
+        img: IEIAOT,
+        link: "https://sccsebio.vercel.app/",
+        alt: "IEI Student Chapter",
+    },
+    {
+        img: CodeChamp,
+        link: "https://code-champs.com/",
+        alt: "Code Champs",
+    },
+    {
+        img: password,
+        link: "https://1password.com/",
+        alt: "1Password",
+    },
+    {
+        img: interviewcake,
+        link: "https://www.interviewcake.com/",
+        alt: "Interview Cake",
+    },
+    {
+        img: LL,
+        link: "https://www.leading-learners.com/",
+        alt: "Leading Learners",
+    },
+    {
+        img: auth,
+        link: "https://auth0.com/",
+        alt: "Auth0",
+    },
+    {
+        img: capitalone,
+        link: "https://www.capitalone.com/",
+        alt: "Capital One",
+    },
+    {
+        img: Deso,
+        link: "https://www.deso.com/",
+        alt: "Deso",
+    },
+    {
+        img: GoDaddy,
+        link: "https://www.godaddy.com/en-in",
+        alt: "GoDaddy",
+    },
+    {
+        img: hedera,
+        link: "https://hedera.com/",
+        alt: "Hedera",
+    },
+    {
+        img: linode,
+        link: "https://www.linode.com/",
+        alt: "Linode",
+    },
+    {
+        img: twilio,
+        link: "https://www.twilio.com/en-us",
+        alt: "Twilio",
+    },
+]
+
+export {gov,sponsors};
