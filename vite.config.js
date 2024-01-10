a23ca497b099c2fc466f5fdc11c63e0817fde33e
Shinjan-saha <shinjansaha02@gmail.com>
2024-01-10 12:09:47 +0530

    VitePWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+       manifest:{
+        icons:[
+          {src : "/pwalogo.png",
+          sizes:"160x160" , 
+          type:"image/png",
+          purpose:"any maskable"
+        },
+        ]
+       }
+  })],
 });
