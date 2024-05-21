1d94ec9805088eafbbdc7347b32c430380753fb3
tamalCodes <tamalcodes@gmail.com>
2024-05-21 23:52:50 +0530

    fix: self desstructing sw

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: "autoUpdate",
+      // selfDestroying: true,
       manifest: {
         short_name: "Hack4Bengal",
         name: "Hack4Bengal",
@@ -24,52 +26,57 @@ export default defineConfig({
         ],
       },
 
-      workbox: {
-        cleanupOutdatedCaches: true,
-        skipWaiting: true,
-        clientsClaim: true,
+      // workbox: {
+      //   cleanupOutdatedCaches: true,
+      //   skipWaiting: true,
+      //   clientsClaim: true,
 
-        runtimeCaching: [
-          {
-            urlPattern: new RegExp(
-              "^https://fonts.(?:googleapis|gstatic).com/(.*)"
-            ),
-            handler: "CacheFirst",
-            options: {
-              cacheName: "google-fonts",
-              expiration: {
-                maxEntries: 30,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            // Cache JS and JSX files with NetworkFirst strategy
-            urlPattern: /\.(?:js|jsx)$/,
-            handler: "NetworkFirst",
-            options: {
-              cacheName: "scripts",
-              expiration: {
-                maxEntries: 60, // Adjust as needed
-                // 30 minutes
-                maxAgeSeconds: 1800,
-              },
-            },
-          },
-          {
-            urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "images",
-              expiration: {
-                maxEntries: 60,
-              },
-            },
-          },
-        ],
-      },
+      //   runtimeCaching: [
+      //     {
+      //       urlPattern: new RegExp(
+      //         "^https://fonts.(?:googleapis|gstatic).com/(.*)"
+      //       ),
+      //       handler: "CacheFirst",
+      //       options: {
+      //         cacheName: "google-fonts",
+      //         expiration: {
+      //           maxEntries: 30,
+      //         },
+      //         cacheableResponse: {
+      //           statuses: [0, 200],
+      //         },
+      //       },
+      //     },
+
+      //     {
+      //       urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+      //       handler: "CacheFirst",
+      //       options: {
+      //         cacheName: "images",
+      //         expiration: {
+      //           maxEntries: 60,
+      //         },
+      //       },
+      //     },
+      //     {
+      //       urlPattern: /\.(?:js|jsx)$/, // Matches all URLs
+      //       handler: "NetworkFirst",
+      //       options: {
+      //         cacheName: "all-content", // Adjust cache name if needed
+      //         cacheableResponse: {
+      //           statuses: [0, 200], // Cache successful responses
+      //         },
+
+      //         // Add a custom cache key based on a versioning mechanism
+      //         cacheKeyUpdater: (request) => {
+      //           const url = request.url;
+      //           const version = "0"; // Replace with your versioning mechanism
+      //           return `${url}-${version}`;
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
     }),
   ],
 });
