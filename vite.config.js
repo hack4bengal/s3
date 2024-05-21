1b3a9549aa56b8245560107977a9c187b27b0176
tamalCodes <tamalcodes@gmail.com>
2024-05-21 21:00:03 +0530

    sw test

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,63 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   manifest: {
-    //     short_name: "Hack4Bengal",
-    //     name: "Hack4Bengal",
-    //     start_url: ".",
-    //     display: "standalone",
-    //     theme_color: "#000000",
-    //     background_color: "#ffffff",
-    //     icons: [
-    //       {
-    //         src: "/pwalogo.png",
-    //         sizes: "160x160",
-    //         type: "image/png",
-    //         purpose: "any maskable",
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      manifest: {
+        short_name: "Hack4Bengal",
+        name: "Hack4Bengal",
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwalogo.png",
+            sizes: "160x160",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+
+        runtimeCaching: [
+          {
+            // Caches Google Fonts with a Cache First strategy.
+            urlPattern: new RegExp(
+              "^https://fonts.(?:googleapis|gstatic).com/(.*)"
+            ),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts",
+              expiration: {
+                maxEntries: 30,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            // Caches images with a Cache First strategy.
+            urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 60,
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
 });
