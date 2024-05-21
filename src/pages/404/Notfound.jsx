73813a56342ca7af5a93f940a94f1cf76591e6a4
tamalCodes <tamalcodes@gmail.com>
2024-05-22 00:10:10 +0530

    fix: cache bust

--- src/pages/404/Notfound.jsx
+++ src/pages/404/Notfound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Notfound = () => {
-  return <div>Notfound</div>;
+  return <div>Notfound - if u see this text then tamal is a good dev.</div>;
 };
 
 export default Notfound;
