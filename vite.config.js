98022d217e0962ac6a1d760e7c52b0f6e46dfc42
tamalCodes <tamalcodes@gmail.com>
2024-05-21 23:25:55 +0530

    fix: workbox

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -22,8 +23,52 @@ export default defineConfig({
           },
         ],
       },
-      injectManifest: {
-        swSrc: "src/sw.js", // Relative path to your sw.js file
+
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+
+        runtimeCaching: [
+          {
+            urlPattern: new RegExp(
+              "^https://fonts.(?:googleapis|gstatic).com/(.*)"
+            ),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts",
+              expiration: {
+                maxEntries: 30,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            // Cache JS and JSX files with NetworkFirst strategy
+            urlPattern: /\.(?:js|jsx)$/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "scripts",
+              expiration: {
+                maxEntries: 60, // Adjust as needed
+                // 30 minutes
+                maxAgeSeconds: 1800,
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 60,
+              },
+            },
+          },
+        ],
       },
     }),
   ],
