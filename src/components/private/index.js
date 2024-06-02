6d2c44365223ff0577ed2f7fca333a95d956f8ad
Andresha56 <rinkyshaw65479@gmail.com>
2024-06-02 13:30:29 +0530

    feat(timeline): added timeline section

--- src/components/private/index.js
+++ src/components/private/index.js
@@ -4,6 +4,7 @@ export const Landing = lazy(() => import("./landing/Landing"));
 export const Marquee = lazy(() => import("./marquee/Marquee"));
 export const About = lazy(() => import("./about/About"));
 export const Timer = lazy(() => import("./timer/Timer"));
+export const TimeLine = lazy(()=>import("./timeline/Timeline"))
 export const Theme = lazy(() => import("./themes/Theme"));
 export const Prizes = lazy(() => import("./prizes/Prizes"));
 export const Sponsors = lazy(() => import("./sponsors/Sponsors"));
