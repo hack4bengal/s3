c952978d7492c82ea163d4239827e43ec077d692
rohit <rhtry.tech@gmail.com>
2024-06-03 23:04:50 +0530

    update: requirements

--- src/pages/404/Notfound.jsx
+++ src/pages/404/Notfound.jsx
@@ -1,15 +1,14 @@
 import React, {useState} from "react";
-
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 import DogeImage from "../../assets/images/404/doggo_img.webp";
 import ReversedDogeImage from "../../assets/images/404/reversed_doggo_img.webp";
-
-import {Button} from "../../components/shared";
 import petHimCircle from "../../assets/images/404/petHimCircle.svg";
-
 import "./Notfound.scss";
+import "../../components/shared/button/Button.scss";
 
 const Notfound = () => {
-  const [isReversed, setIsReversed] = useState(true);
+  const [isReversed, setIsReversed] = useState(false);
   const [circlePosition, setCirclePosition] = useState({x: 0, y: 0});
   const [isHovering, setIsHovering] = useState(false);
 
@@ -41,52 +40,43 @@ const Notfound = () => {
   };
 
   return (
-    <>
-      <div className="pageNotFound__container" id="pageNotFound">
-        <div className="pageNotFound__content">
-          <div className="pageNotFound__desc">
-            <h2>404!</h2>
-            <p>
-              This page was not found, but you can stay here and pet our dogo
-            </p>
-
-            <div className="back_to_home_page_btn">
-              <Button type="outline" to={"/"}>
-                Back to Homepage
-              </Button>
-            </div>
+    <div className="pageNotFound__container" id="pageNotFound">
+      <div className="pageNotFound__content">
+        <div className="pageNotFound__desc">
+          <h2>404!</h2>
+          <p>This page was not found, but you can stay here and pet our dogo</p>
+          <div className="back_to_home_page_btn">
+            <Link to={"/"} className="button outline">
+              Back to Homepage
+            </Link>
           </div>
-
-          <div className="notfound__doge_container">
-            <div
-              className="doge__image"
-              onClick={toggleImage}
-              onMouseMove={handleMouseMove}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              <img
-                src={isReversed ? ReversedDogeImage : DogeImage}
-                alt="Doge"
-              />
-              {isHovering && (
-                <div
-                  className="notfound__petDogeCircle"
-                  style={{
-                    position: "absolute",
-                    top: `${circlePosition.y - 75}px`,
-                    left: `${circlePosition.x + 15}px`,
-                    pointerEvents: "none",
-                  }}
-                >
-                  <img src={petHimCircle} alt="Pet Him Circle" />
-                </div>
-              )}
-            </div>
+        </div>
+        <div className="notfound__doge_container">
+          <div
+            className="doge__image"
+            onClick={toggleImage}
+            onMouseMove={handleMouseMove}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <img src={isReversed ? ReversedDogeImage : DogeImage} alt="Doge" />
+            {isHovering && (
+              <div
+                className="notfound__petDogeCircle"
+                style={{
+                  position: "absolute",
+                  top: `${circlePosition.y - 75}px`,
+                  left: `${circlePosition.x + 15}px`,
+                  pointerEvents: "none",
+                }}
+              >
+                <img src={petHimCircle} alt="Pet Him Circle" />
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
